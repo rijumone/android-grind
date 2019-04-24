784d1f43f20da5abbd3e9b4a35a03ab62e61d771
Riju <mailmeonriju@gmail.com>
2019-04-25 01:58:24 +0530

    initial commit

---
+++ NFSNoLimitsGold/start.py
@@ -0,0 +1,89 @@
+# game is launched and ready
+
+import time
+import subprocess
+import pytesseract
+from PIL import Image, ImageOps
+
+def main():
+
+	while True:
+		# swipe screen to corner to ensure image has nothing beneath it
+		for _ in range(2):
+			# twice to make sure
+			subprocess.call(['adb', 'shell', 'input', 'touchscreen', 'swipe', '1200', '600', '200', '200'])
+
+		# take a screenshot to check button exists
+		_capture_and_pull_screenshot()
+
+		print('current cash: ',_check_assets('cash'))
+		print('current gold: ',_check_assets('gold'))
+
+		# crop the image to extract only the portion with text
+		ImageOps.crop(Image.open('tmp/screen.png'), (860, 650, 120, 0)).save('tmp/screen_cropped.png')
+
+		# feed this image to OCR to get the words
+		if pytesseract.image_to_string(Image.open('tmp/screen_cropped.png')).lower().strip().split()[0] != 'earn':
+			print('didn\'t match "earn xxxx", breaking')
+			break
+		
+		# tap on the 'earn gold' icon
+		subprocess.call(['adb', 'shell', 'input', 'tap', '1260', '710'])
+		
+		# attempt to identify type of ad and time to sleep
+		pos_X_btn, sleep_sec = _get_pos_and_sec()
+		# wait 30 seconds plus few seconds extra for the ad to complete playing
+		print('sleeping for {} seconds'.format(sleep_sec))
+		time.sleep(sleep_sec)
+		print('sleeping for 2 seconds')
+		time.sleep(2)
+		
+		# tap on 'X' button
+		subprocess.call(['adb', 'shell', 'input', 'tap', pos_X_btn[0], pos_X_btn[1]])
+
+		# keep checking if screen returned after ad
+		returned_after_ad = False
+		while not returned_after_ad:
+			# capture the screen
+			_capture_and_pull_screenshot()
+			# crop it
+			ImageOps.crop(Image.open('tmp/screen.png'), (1040, 630, 70, 20)).save('tmp/screen_cropped.png')
+			if pytesseract.image_to_string(Image.open('tmp/screen_cropped.png')).lower().strip() != 'continue':
+				print('sleeping for 3 seconds while waiting for screen to return from ad')
+				time.sleep(3)
+			else:
+				returned_after_ad = True
+
+		# tap on 'continue' button
+		subprocess.call(['adb', 'shell', 'input', 'tap', '1250', '690'])
+
+		# give it some time to return to main screen
+		time.sleep(5)
+		print('=====================================================================')
+		# break
+
+def _capture_and_pull_screenshot():
+	subprocess.call(['adb', 'shell', 'screencap', '/sdcard/screen.png'])
+	subprocess.call(['adb', 'pull', '/sdcard/screen.png', 'tmp/screen.png'])
+6
+def _check_assets(asset_type):
+	''' check appropriate asset_type from exiting image and return a string'''
+
+	if asset_type == 'gold':
+		crop_tpl = (1020, 0, 195, 66)
+	elif asset_type == 'cash':
+		crop_tpl = (860, 0, 310, 660)
+
+	ImageOps.crop(Image.open('tmp/screen.png'), crop_tpl).save('tmp/screen_cropped.png')
+	return pytesseract.image_to_string(Image.open('tmp/screen_cropped.png')).lower().strip()
+
+def _get_pos_and_sec():
+	for crop_tpl in [(953, 20, 80, 660), ]:
+		ImageOps.crop(Image.open('tmp/screen.png'), crop_tpl).save('tmp/screen_cropped.png')
+		result_lst = pytesseract.image_to_string(Image.open('tmp/screen_cropped.png')).lower().strip().split()
+		if 'seconds' in result_lst or 'remaining' in result_lst:
+			return (1260, 10), result_lst[0]
+
+	return None
+if __name__ == '__main__':
+	main()
---
+++ README.md
@@ -0,0 +1,16 @@
+# android-grind
+
+Project to grind your way to faboulous riches. On Android games and such.
+
+##### Common steps for all games/apps (device)
+- Install the game/app on your device
+- Connect to your device over adb (wirelessly works a bit slow in my experience since screenshots will be transferred)
+
+##### Common steps for all games/apps (machine)
+- Create a virtualenv
+- Install the dependencies outlined in `requirements.txt`
+`pip install -r requirements.txt`
+- `mkdir tmp`
+
+##### Need For Speed - No Limits
+- Launch the game on your device
